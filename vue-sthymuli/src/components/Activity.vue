8e9cdd13589b855dd271071375cf30e482993ada
steiner <jennyfer.steiner@outlook.com>
2026-06-30 00:51:04 +0200

    Add activity demo (images) with easy and medium version, different led colors and OR with 3 inputs

--- vue-sthymuli/src/components/Activity.vue
+++ vue-sthymuli/src/components/Activity.vue
@@ -1,13 +1,36 @@
 <script lang="ts">
 import Challenges from './Challenges.vue';
+
+import easyActivity from '../assets/Easy.svg';
+import mediumActivity from '../assets/and.svg';
+import medium3InOrActivity from '../assets/OR 3in.svg';
+import mediumRGBActivity from '../assets/RGB leds.svg';
+import { loadEnv } from 'vite';
+
 export default {
+    data() {
+        return {
+            activities: [
+                {
+                    name: 'pres',
+                    imgs: [
+                        easyActivity,
+                        mediumActivity,
+                        medium3InOrActivity,
+                        mediumRGBActivity
+                    ]
+                }
+            ],
+            currentActivity: 0,
+        }
+    }
 }
 </script>
 
 <template>
-    <div>
-        <h1>Activity {{ $route.params.activityID }}</h1>
-        <!-- <img src="" alt="" /> -->
+    <div class="Challenges block">
+        <!-- <h1>Activity {{ $route.params.activityID }}</h1> -->
+        <img @click="currentActivity = (currentActivity + 1) % activities[0]!.imgs.length" :src="activities[0]?.imgs[currentActivity]" alt="Activity prototype with puzzle pieces"/>
     </div>
 </template>
 
